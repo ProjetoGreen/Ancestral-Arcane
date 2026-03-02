a53cb6e040c29d77276189c152ad5c94a1ce5b31
dnettoRaw <contact@dnetto.dev>
2026-03-02 01:01:25 +0100

    feat: show all rune progression states directly in creative tab

--- src/main/java/com/ancestralarcane/registry/AncestralArcaneCreativeTab.java
+++ src/main/java/com/ancestralarcane/registry/AncestralArcaneCreativeTab.java
@@ -18,7 +18,57 @@ public class AncestralArcaneCreativeTab {
                     .title(Component.translatable("itemGroup.dntr"))
                     .icon(() -> new ItemStack(AncestralArcaneItems.RUNE.get()))
                     .displayItems((parameters, output) -> {
+                        // 1. Empty Rune
                         output.accept(AncestralArcaneItems.RUNE.get());
+
+                        // 2. Crude Rune
+                        ItemStack crudeRune = new ItemStack(AncestralArcaneItems.RUNE.get());
+                        net.minecraft.nbt.CompoundTag cdData = new net.minecraft.nbt.CompoundTag();
+                        net.minecraft.nbt.CompoundTag cdRune = new net.minecraft.nbt.CompoundTag();
+                        cdRune.putInt("crude", 1);
+                        cdData.put("rune", cdRune);
+                        com.ancestralarcane.data.CustomDataUtil.setAncestralArcaneData(crudeRune, cdData);
+                        output.accept(crudeRune);
+
+                        // 3. Generic Inscribed Rune
+                        ItemStack inscribedRune = new ItemStack(AncestralArcaneItems.RUNE.get());
+                        net.minecraft.nbt.CompoundTag inscData = new net.minecraft.nbt.CompoundTag();
+                        net.minecraft.nbt.CompoundTag inscRune = new net.minecraft.nbt.CompoundTag();
+                        inscRune.putInt("empty", 0);
+                        inscRune.putInt("lvl", 1);
+                        inscRune.putString("spell", "flame");
+                        inscData.put("rune", inscRune);
+                        com.ancestralarcane.data.CustomDataUtil.setAncestralArcaneData(inscribedRune, inscData);
+                        output.accept(inscribedRune);
+
+                        // 4. Upgraded Rune (empty but upgraded)
+                        ItemStack upgRune = new ItemStack(AncestralArcaneItems.RUNE.get());
+                        net.minecraft.nbt.CompoundTag upgData = new net.minecraft.nbt.CompoundTag();
+                        net.minecraft.nbt.CompoundTag uRune = new net.minecraft.nbt.CompoundTag();
+                        uRune.putInt("empty", 1);
+                        net.minecraft.nbt.CompoundTag uTag = new net.minecraft.nbt.CompoundTag();
+                        uTag.putString("type", "blaze");
+                        uTag.putInt("level", 1);
+                        uRune.put("upgrade", uTag);
+                        upgData.put("rune", uRune);
+                        com.ancestralarcane.data.CustomDataUtil.setAncestralArcaneData(upgRune, upgData);
+                        output.accept(upgRune);
+
+                        // 5. Upgraded + Inscribed Rune
+                        ItemStack uiRune = new ItemStack(AncestralArcaneItems.RUNE.get());
+                        net.minecraft.nbt.CompoundTag uiData = new net.minecraft.nbt.CompoundTag();
+                        net.minecraft.nbt.CompoundTag uiR = new net.minecraft.nbt.CompoundTag();
+                        uiR.putInt("empty", 0);
+                        uiR.putInt("lvl", 1);
+                        uiR.putString("spell", "flame");
+                        net.minecraft.nbt.CompoundTag uiTag = new net.minecraft.nbt.CompoundTag();
+                        uiTag.putString("type", "blaze");
+                        uiTag.putInt("level", 1);
+                        uiR.put("upgrade", uiTag);
+                        uiData.put("rune", uiR);
+                        com.ancestralarcane.data.CustomDataUtil.setAncestralArcaneData(uiRune, uiData);
+                        output.accept(uiRune);
+
                         output.accept(AncestralArcaneItems.ARCANE_RESIDUE.get());
                         output.accept(AncestralArcaneItems.INTENT_INK_FLAME.get());
                         output.accept(AncestralArcaneItems.INTENT_INK_CHANNELING.get());
